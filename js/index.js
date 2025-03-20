78a2f39e6e298e963e1472dc028485fee853dcf3
kr1s10 <kr1s10.khud@gmail.com>
2025-03-20 19:45:11 +0200

    feat: add accordion functionality

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-import { ACCESS_TOKEN, query, URL } from "./data.js";
+import { ACCESS_TOKEN, faqData, query, URL } from "./data.js";
 import Product from "./product.js";
 
-fetchProducts(URL);
+document.addEventListener('DOMContentLoaded', () => {
+  fetchProducts(URL);
+  renderAccordion(faqData, 'accordion', 'accordion-template');
+});
 
 async function fetchProducts (url) {
   try {
@@ -35,4 +38,42 @@ function renderProducts(products, containerId, templateId) {
     const productElement = product.generateMarkupElement();
     container.append(productElement);
   });
+}
+
+function renderAccordion(items, containerId, templateId) {
+  const container = document.getElementById(containerId);
+  const accordionTemplate = document.getElementById(templateId);
+
+  items.forEach((item, idx) => {
+    const accordionItem = createAccordionItem(item, accordionTemplate, container, idx === 0);
+    container.append(accordionItem);
+  });
+}
+
+function createAccordionItem({title, content}, template, container, isFirstItem) {
+  const clone = template.content.cloneNode(true);
+  const accordionItem = clone.querySelector(".accordion__item");
+  const accordionButton = accordionItem.querySelector(".accordion__header")
+  const titleElement = accordionItem.querySelector(".accordion__header-text");
+  const contentElement = accordionItem.querySelector(".accordion__text");
+
+  titleElement.textContent = title;
+  contentElement.textContent = content;
+
+  if (isFirstItem) accordionButton.setAttribute('aria-expanded', 'true');
+
+  accordionButton.addEventListener('click', () => toggleAccordion(accordionButton, container));
+
+  return accordionItem;
+}
+
+function toggleAccordion(button, container) {
+  const isExpanded = button.getAttribute('aria-expanded') === 'true';
+  const allButtons = container.querySelectorAll('.accordion__header');
+
+  allButtons.forEach(item => item.setAttribute('aria-expanded', 'false'));
+
+  if (!isExpanded) {
+    button.setAttribute('aria-expanded', 'true');
+  }
 }
